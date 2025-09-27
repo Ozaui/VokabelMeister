97fd554dd40e5e1b99e7b8b93a2fa207930afd71
Özay Melih Yıldız <102221334+Ozaui@users.noreply.github.com>
2025-09-27 12:15:19 +0300

    vercel bugfixing 9

--- VokabelMeister/vite.config.ts
+++ VokabelMeister/vite.config.ts
@@ -5,33 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
-  build: {
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split(".");
-          const ext = info[info.length - 1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
-            return `assets/images/[name]-[hash][extname]`;
-          }
-          if (/css/i.test(ext)) {
-            return `assets/css/[name]-[hash][extname]`;
-          }
-          if (/woff|woff2|eot|ttf|otf/i.test(ext)) {
-            return `assets/fonts/[name]-[hash][extname]`;
-          }
-          return `assets/[name]-[hash][extname]`;
-        },
-        chunkFileNames: "assets/js/[name]-[hash].js",
-        entryFileNames: "assets/js/[name]-[hash].js",
-      },
-    },
-  },
-  server: {
-    headers: {
-      "Cache-Control": "no-cache",
-    },
-  },
 });
