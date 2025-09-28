c7bd261d689f3137dc2981e82d67a4f83f027c4d
Özay Melih Yıldız <102221334+Ozaui@users.noreply.github.com>
2025-09-28 21:33:46 +0300

    bugfixing

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -26,7 +26,7 @@ const connectDB = async () => {
 
 app.use(
   cors({
-    origin: "https://vokabel-meister-xkjc.vercel.app",
+    origin: "https://vokabelmeister.vercel.app/",
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
